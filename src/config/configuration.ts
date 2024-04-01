4688bef98cda318055cd51f2412b8d1950ef6d14
anhnvt <anhnguyenviettuan@admicro.vn>
2024-04-01 11:13:00 +0700

    edit config db

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,8 +2,8 @@ export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
     database: {
         host: process.env.DATABASE_HOST || '127.0.0.1',
-        port: parseInt(process.env.DATABASE_PORT, 10) || 3307,
-        database: process.env.DATABASE_NAME || 'eco',
+        port: parseInt(process.env.DATABASE_PORT, 10) || 3304,
+        database: process.env.DATABASE_NAME || 'eco_xt',
         username: process.env.DATABASE_USERNAME || 'root',
         password: process.env.DATABASE_PASSWORD || '',
     },
